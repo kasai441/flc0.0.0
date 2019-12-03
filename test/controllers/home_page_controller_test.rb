fd539293f2fd53f4493dd9e3b89b80e41519f8f8
kasai441 <20lions@gmail.com>
2019-12-03 16:05:24 +0900

    Add views

--- test/controllers/home_page_controller_test.rb
+++ test/controllers/home_page_controller_test.rb
@@ -4,6 +4,7 @@ class HomePageControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get home_page_show_url
     assert_response :success
+    assert_select "title", "ホーム｜Flashcards"
   end
 
 end
