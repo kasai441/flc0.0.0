e0f79bbd3507bb92dda627beb735fa0785bfb061
kasai441 <20lions@gmail.com>
2019-12-03 16:18:28 +0900

    set root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'home_page/show'
-
+  root 'home_page#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/home_page_controller_test.rb
+++ test/controllers/home_page_controller_test.rb
@@ -7,4 +7,9 @@ class HomePageControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "ホーム｜Flashcards"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
 end
