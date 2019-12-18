32d1f1700ea31b20446de4fe9b3ab97843a7f8ce
kasai441 <20lions@gmail.com>
2019-12-18 09:09:16 +0000

    adjust quizcards empty pattern

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,12 +2,12 @@ class HomePageController < ApplicationController
   def index
     if logged_in?
       @user = current_user
-      @quizcard = @user.quizcards.first
+      @quizcard = @user.quizcards.first if @user.quizcards.any?
       @quizcards = @user.quizcards.paginate(page: params[:page])
       render 'show'
     else
       @user = User.first
-      @quizcard = @user.quizcards.first
+      @quizcard = @user.quizcards.first if @user.quizcards.any?
       @quizcards = @user.quizcards.paginate(page: params[:page])
       render 'index'
     end
